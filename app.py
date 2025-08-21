537732081987b86e2c563ad9bbb0d44cb9700f9c
David Vroom Duke <dduke@netflix.com>
2025-08-21 04:39:06 +0000

    Update webapp to trigger CI/CD

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ async def root():
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Hello from AWS ECS</title>
+        <title>Hello from AWS ECS!</title>
         <style>
             body {
                 font-family: Arial, sans-serif;
